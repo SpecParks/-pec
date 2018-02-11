cc59b4f9f2e0d8c3d611bfb26fb1fd60ea7d1b86
Brendan Dugan <bdugan14@users.noreply.github.com>
2018-02-11 14:08:10 -0800

    Add donation form page

--- src/pages/donation-form/donation-form.ts
+++ src/pages/donation-form/donation-form.ts
@@ -15,6 +15,11 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class DonationFormPage {
 
+  formData = {
+    name: '',
+    email: '',
+  };
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
